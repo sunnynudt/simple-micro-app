25f1a54b4994370dd7b6231a1683956be583d961
lucky <995783795@qq.com>
2025-04-20 15:04:31 +0000

    update dev

--- examples/base/vue2/vue.config.js
+++ examples/base/vue2/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    disableHostCheck: true,
+    public: '0.0.0.0' // 允许容器外部访问
+  }
 })
